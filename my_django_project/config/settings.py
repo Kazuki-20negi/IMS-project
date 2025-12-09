200cc91deda4869bf08d8902056d490ca483a98d
20本ネギ <s.kazuki0111@gmail.com>
2025-12-09 23:00:03 +0900

    エラー解決

--- my_django_project/config/settings.py
+++ my_django_project/config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # 開発環境での静的ファイルの配置場所
 STATICFILES_DIRS = [
