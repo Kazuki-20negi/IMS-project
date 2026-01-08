30198f553f8c2e5e13d76fe6691babed6cb98398
20本ネギ <s.kazuki0111@gmail.com>
2026-01-09 00:13:57 +0900

    make models.py

--- audiograms/models.py
+++ audiograms/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class data(models.Model):
+    original_file=models.ImageField()
+    filname=models.CharField()
+    exam_date=models.DateField()
+    need_review=models.BooleanField()
+    memo=models.TextField(blank=True, null=True)
+    created_at=models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return data.filname
+
