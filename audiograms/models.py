08ceb8d9ae8451ca05e0b1a57c435f200e6c8a7c
20本ネギ <s.kazuki0111@gmail.com>
2026-01-09 00:18:11 +0900

    修正

--- audiograms/models.py
+++ audiograms/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class data(models.Model):
-    original_file=models.ImageField()
-    filname=models.CharField()
-    exam_date=models.DateField()
-    need_review=models.BooleanField()
+class Audiogram(models.Model):
+    original_file=models.ImageField(upload_to='audiograms/')
+    filename=models.CharField(max_length=255)
+    exam_date=models.DateField(null=True, blank=True)
+    need_review=models.BooleanField(default=False)
     memo=models.TextField(blank=True, null=True)
-    created_at=models.DateTimeField(auto_created=True)
+    created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return data.filname
+        return self.filename
 
