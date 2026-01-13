e8230c61dba84575993e7d1b08ffda9dfa0dc8d0
20本ネギ <s.kazuki0111@gmail.com>
2026-01-14 00:32:14 +0900

    Update urls.py

--- audiograms/urls.py
+++ audiograms/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('upload/', views.upload_audiogram, name='upload_audiogram'),
-    path("", views.audiogram_list, name="audiograms")
+    path("", views.audiogram_list, name="audiograms"),
+    path('edit/<int:audiogram_id>/', views.audiogram_edit, name='audiogram_edit'),
 ]
